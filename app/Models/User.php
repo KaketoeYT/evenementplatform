8efa8e0edabd31f02f99f1100738402d9507ef23
cemil <cemil3456@icloud.com>
2026-04-15 13:55:51 +0100

    feat: add ticket relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Ticket;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -60,5 +61,9 @@ class User extends Authenticatable
             ->explode(' ')
             ->map(fn(string $name) => Str::of($name)->substr(0, 1))
             ->implode('');
+    }
+    public function ticket()
+    {
+        return $this->hasMany(Ticket::class);
     }
 }
