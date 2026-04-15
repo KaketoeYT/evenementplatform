ea1f676f031e1fafee3b45fd8d99a4277ba7a0e0
cemil <cemil3456@icloud.com>
2026-04-15 10:17:32 +0100

    Add additional fields to User model and factory for enhanced user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'rol' => fake(),
+            'phonenumber' => fake()->phonenumber(),
+            'emergency_contact' => fake()->phonenumber(),
+            'zipcode' => fake()->postcode(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
