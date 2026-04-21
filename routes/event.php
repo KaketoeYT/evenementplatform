e9110d34c65243d50a6bfdac9b1eb38043f6df45
KaketoeYT <Thijsderogee@gmail.com>
2026-04-21 13:18:25 +0200

    added proper authentication to event routes and removed duplicates in web

--- routes/event.php
+++ routes/event.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 
-Route::middleware('auth')->group(function () {
+// These routes are only accessible for users with the 'organizer' role.
+Route::middleware(['auth', 'check.role:organizer'])->group(function () {
     Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
     Route::post('/events', [EventController::class, 'store'])->name('events.store');
     Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
