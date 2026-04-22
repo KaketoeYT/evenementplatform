2d19368bf7080e57d27f23c77be8663a2d254e89
KaketoeYT <Thijsderogee@gmail.com>
2026-04-22 13:53:14 +0200

    changed back button to actually redirect back

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -50,7 +50,7 @@
             </button>
         </form>>
         {{-- Back link --}}
-        <a href="{{ route('events.index') }}"
+        <a href="{{ redirect()->back() }}"
             class="inline-block mt-4 text-indigo-600 hover:text-indigo-800 font-medium transition-colors duration-200">
             ← Terug naar alle evenementen
 
