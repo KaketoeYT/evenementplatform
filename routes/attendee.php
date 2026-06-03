bad46e564149ea0373913cdccc247fb37adcf663
KaketoeYT <Thijsderogee@gmail.com>
2026-06-03 09:28:11 +0200

    reorganised routes to be more tidy

--- routes/attendee.php
+++ routes/attendee.php
@@ -10,12 +10,12 @@ Route::middleware(['auth', 'check.role:organizer'])->group(function () {
     
     Route::get('/attendees/create', [AttendeeController::class, 'create'])->name('attendee.create');
     Route::post('/attendees', [AttendeeController::class, 'store'])->name('attendee.store');
+
     Route::get('/attendees/{id}/edit', [AttendeeController::class, 'edit'])->name('attendee.edit');
     Route::put('/attendees/{id}', [AttendeeController::class, 'update'])->name('attendee.update');
-    Route::delete('/attendees/{id}', [AttendeeController::class, 'destroy'])->name('attendee.destroy');
 
-    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])
-        ->name('attendee.ticket.destroy');
+    Route::delete('/attendees/{id}', [AttendeeController::class, 'destroy'])->name('attendee.destroy');
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('attendee.ticket.destroy');
 });
 
 Route::get('/attendees/{id}', [AttendeeController::class, 'show'])->name('attendee.show');
--- routes/web.php
+++ routes/web.php
@@ -9,32 +9,28 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrganizerRequestController;
 
-// TEST
-Route::get('/organizer_request', [OrganizerRequestController::class, 'index'])->name('organizer_request.index');
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
+// login functions
 Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', [Settings\ProfileController::class, 'edit'])->name('settings.profile.edit');
     Route::put('settings/profile', [Settings\ProfileController::class, 'update'])->name('settings.profile.update');
     Route::delete('settings/profile', [Settings\ProfileController::class, 'destroy'])->name('settings.profile.destroy');
+
     Route::get('settings/password', [Settings\PasswordController::class, 'edit'])->name('settings.password.edit');
     Route::put('settings/password', [Settings\PasswordController::class, 'update'])->name('settings.password.update');
+
     Route::get('settings/appearance', [Settings\AppearanceController::class, 'edit'])->name('settings.appearance.edit');
 });
 
+// Admin functions
 Route::middleware(['auth', 'check.role:admin'])->group(function () {
     Route::get('administrator/user_view', [Settings\ProfileController::class, 'user_view'])->name('administrator.user.view');
     Route::post('administrator/user_view', [Settings\ProfileController::class, 'update_roles'])->name('administrator.user.update');
+
+    Route::post('administrator/user_deactivate/{userId}', [Settings\ProfileController::class, 'deactivate_user'])->name('administrator.user.deactivate');
     Route::post('administrator/user_deactivate/{userId}', [Settings\ProfileController::class, 'deactivate_user'])->name('administrator.user.deactivate');
     Route::get('mails/password_reset/{userId}', [Settings\ProfileController::class, 'sendPasswordResetMail'])->name('mails.password_reset');
 
@@ -42,14 +38,9 @@ Route::middleware(['auth', 'check.role:admin'])->group(function () {
     Route::delete('administrator/application_view', [OrganizerRequestController::class, 'destroy'])->name('organizer_request.destroy');
 });
 
-// Unauthorized routes
-
-Route::get('/attendees', [AttendeeController::class, 'index'])
-    ->name('attendee.index');
 
 Route::post('/events/{event}/toggle-registration', [EventController::class, 'toggleRegistration'])->name('events.toggleRegistration');
 
-Route::post('administrator/user_deactivate/{userId}', [Settings\ProfileController::class, 'deactivate_user'])->name('administrator.user.deactivate');
 Route::get('mails/password_reset/{userId}', [Settings\ProfileController::class, 'sendPasswordResetMail'])->name('mails.password_reset');
 
 require __DIR__ . '/auth.php';
